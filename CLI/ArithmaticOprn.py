922040954a28cf2fd7d70d4ba31e9a5ca4666987
ashish1997it <sondagarashish@gmail.com>
2018-08-02 02:27:17 +0530

    arith

---
+++ CLI/ArithmaticOprn.py
@@ -0,0 +1,9 @@
+int1 = int(input('Enter 1st digit: '))
+int2 = int(input('Enter 2st digit: '))
+
+print('Addition= ', int1+int2)
+print('Subtraction= ', int1-int2)
+print('Multiplication= ', int1*int2)
+print('Division= ', int1/int2)
+print('Modulus= ', int1%int2)
+
